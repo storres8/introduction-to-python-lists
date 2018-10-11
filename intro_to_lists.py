2dd8618c3e9c395fdbeba2e659913e60bbe3d9d6
storres8 <stevetorres7562@gmail.com>
2018-10-10 20:12:45 -0400

    created, indexed, and sliced some lists

---
+++ intro_to_lists.py
@@ -0,0 +1,14 @@
+my_list=[1,2,3]
+
+new_list=["hello", 100, 23.2]
+
+print(len(my_list))
+# prints 3 since there are 3 things in my list
+
+print(new_list)
+# prints out new_list and shows that we have can have different data types
+# all in the same list
+
+print(new_list[1])
+# prints out 100 since thats is the second value in my list
+print(new_list[0:3])
